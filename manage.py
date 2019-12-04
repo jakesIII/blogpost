c0d021f1a5551841702daa15ba1eac1e993b440e
jakesIII <jayruma@yahoo.com>
2019-12-04 05:40:43 +0300

    migration bug resolved

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User
+from app.models import User, Blog, Comments
 from flask_migrate import Migrate, MigrateCommand
 # import instances
 app=create_app('production')
-app=create_app('development')
+# app=create_app('development')
 manager=Manager(app)
 migrate=Migrate(app, db)
 manager.add_command('db', MigrateCommand)
@@ -12,7 +12,7 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict (app=app, db=db, User=User)
+    return dict (app=app, db=db, User=User, Blog=Blog, Comments=Comments)
 
 if __name__=='__main__':
     manager.run()
