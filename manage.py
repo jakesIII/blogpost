2adb768e0a69162678d07d931566816e4f046615
jakesIII <jayruma@yahoo.com>
2019-12-04 05:57:09 +0300

    add a delete button to the comments

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ from flask_script import Manager, Server
 from app.models import User, Blog, Comments
 from flask_migrate import Migrate, MigrateCommand
 # import instances
-app=create_app('production')
-# app=create_app('development')
+# app=create_app('production')
+app=create_app('development')
 manager=Manager(app)
 migrate=Migrate(app, db)
 manager.add_command('db', MigrateCommand)
