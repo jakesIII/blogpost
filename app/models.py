f7685cb7cc2c92473af79972c772f712a35559ba
jakesIII <jayruma@yahoo.com>
2019-12-04 05:52:40 +0300

    add verify password function to check password

--- app/models.py
+++ app/models.py
@@ -28,7 +28,7 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
     # verify
     def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash( self.password_hash, password)
 
 
     def __repr__(self):
